110d5022f6cc46cdda5c12d6c1a316a403605f37
laithalkhraisha <144603563+laithalkhraisha@users.noreply.github.com>
2023-12-25 14:58:31 +0300

    Delete Models directory

--- Models/city.js
+++
@@ -1,9 +0,0 @@
-const {pool} = require('../db/db');
-
-class paymentModel {
-    async getAllcity() {
-      const result = await pool.query('SELECT id, name, image  FROM public.citys;');
-      return result.rows;
-    }
-}
-module.exports = new paymentModel();
